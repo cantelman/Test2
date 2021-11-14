d677746378639fe69a0019c8992a316fd9195064
cantelman <82101041+cantelman@users.noreply.github.com>
2021-11-14 22:01:09 +0300

    Add files via upload

---
+++ Python Çalışma.py
@@ -0,0 +1,13 @@
+x = 34 - 23  # A comment
+y = "Hello" # Another one.
+z = 3.45
+if z == 3.45 or y == "Hello"
+   x = x + 1
+   y = y + "World" # String concat
+print x
+print y
+for i in range(5):
+    print(i)
+    if i>2:
+        print('Bigger than 2')
+    print('Done with i',i)
